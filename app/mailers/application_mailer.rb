308326aa23ad6266c9466eff8b639add93e6702b
Mohammad <mzarifi@blackberry.com>
2017-09-26 13:56:30 -0400

    chapter 12

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@example.com'
   layout 'mailer'
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
+
 end
